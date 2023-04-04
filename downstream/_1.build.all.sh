e28b2ab919db8c9f2e2236675407ed8107433241
thefoundation-builder <actions@github.com>
2023-04-04 20:16:38 +0000

    Update from https://github.com/TheFoundation/hardened-dropbear/commit/5d68343ef77f63aa70beea0d14cf28776f04801f

--- downstream/_1.build.all.sh
+++ downstream/_1.build.all.sh
@@ -41,7 +41,7 @@ mkdir build
     cp ${startdir}/build-bear.sh . -v
     test -e ccache.tgz && rm ccache.tgz
     docker export $(docker create --name cicache_${IMAGETAG//[:\/]/_}_${TARGETARCH} ${IMAGETAG}_${TARGETARCH}_builder /bin/false ) |tar xv ccache.tgz ;docker rm cicache_${IMAGETAG//[:\/]/_}_${TARGETARCH}
-     test -e ccache.tgz ||    (  (echo FROM ${IMAGETAG}_${TARGETARCH}_builder;echo RUN echo yocacheme) | timeout 5000 time docker buildx build  --output=type=local,dest=/tmp/buildout_${IMAGETAG}_${TARGETARCH}_builder  --progress plain --network=host --memory-swap -1 --memory 1024 --platform=${BUILDARCH}   --cache-from ${IMAGETAG}_${TARGETARCH}_buildcache_baseimage  --cache-from ${IMAGETAG}_${TARGETARCH}_baseimage --cache-from ${IMAGETAG}_${TARGETARCH}_builder --cache-from ${IMAGETAG}_${TARGETARCH}_buildcache -t  ${IMAGETAG}_${TARGETARCH}_builder $buildstring -f - ) ;
+     test -e ccache.tgz ||    (  (echo FROM ${IMAGETAG}_${TARGETARCH}_builder;echo RUN echo yocacheme) | timeout 5000 time docker buildx build  --output=type=local,dest=/tmp/buildout_${IMAGETAG}_${TARGETARCH}_builder  --progress plain --network=host --memory-swap -1 --memory 1024 --platform=${BUILDARCH}   --cache-from ${IMAGETAG}_${TARGETARCH}_buildcache_baseimage  --cache-from ${IMAGETAG}_${TARGETARCH}_baseimage --cache-from ${IMAGETAG}_${TARGETARCH}_builder --cache-from ${IMAGETAG}_${TARGETARCH}_buildcache -t  ${IMAGETAG}_${TARGETARCH}_extract $buildstring -f - ) ;
      test -e /tmp/buildout_${IMAGETAG}_${TARGETARCH}_builder && test -e /tmp/buildout_${IMAGETAG}_${TARGETARCH}/ccache.tgz && mv /tmp/buildout_${IMAGETAG}_${TARGETARCH}/ccache.tgz .
      test -e /tmp/buildout_${IMAGETAG}_${TARGETARCH}_builder && rm -rf "/tmp/buildout_${IMAGETAG}_${TARGETARCH}"    
     test -e ccache.tgz || ( mkdir .tmpempty ;echo 123 .tmpempty/file;tar cvzf ccache.tgz .tmpempty )
@@ -59,7 +59,7 @@ echo timeout 5000 time docker buildx build  --output=type=registry,push=true --p
      grep -v -e COPY -e build-bear "${DFILENAME}"  > "${DFILENAME}_baseimage" 
      mkdir builder_baseimage
      mkdir  builder_baseimage/build
-     mv "${DFILENAME}_baseimage" builder_baseimage/"${DFILENAME}_baseimage"
+     mv "${DFILENAME}_baseimage" "builder_baseimage/${DFILENAME}_baseimage"
      (   cd builder_baseimage/;   timeout 5000 time docker buildx build  --output=type=registry,push=true --push  --progress plain --network=host --memory-swap -1 --memory 1024 --platform=${BUILDARCH} --cache-to ${IMAGETAG}_${TARGETARCH}_buildcache_baseimage --cache-from ${IMAGETAG}_${TARGETARCH}_buildcache_baseimage  --cache-from ${IMAGETAG}_${TARGETARCH}_baseimage --cache-from ${IMAGETAG}_${TARGETARCH}_builder --cache-from ${IMAGETAG}_${TARGETARCH}_buildcache -t  ${IMAGETAG}_${TARGETARCH}_baseimage $buildstring -f "${DFILENAME}_baseimage" )
      rm -rf builder_baseimage 
      # second image name _builder is the full thingy ( will be large . .)
